a2f148484071b4ca202ea59eb698107d2ca0ed7b
Whatdoyumin <lym4963@naver.com>
2025-07-01 15:16:42 +0900

    fix: 프로필 이미지 변경 시 UI에 바로 변경되도록 수정

--- frontend/src/components/layouts/menu/AccountMenuItem.vue
+++ frontend/src/components/layouts/menu/AccountMenuItem.vue
@@ -1,8 +1,13 @@
 <!-- 계정 메뉴 그룹 컴포넌트 -->
 <script setup>
-const props = defineProps({ username: String });
+import { useAuthStore } from '@/stores/auth'; // 인증 스토어 가져오기
+import { computed } from 'vue';
 
-const avatar = `/api/member/${props.username}/avatar`;
+const props = defineProps({ username: String });
+const auth = useAuthStore();
+
+//const avatar = `/api/member/${props.username}/avatar`;
+const avatar = computed(() => auth.avatarUrl);
 </script>
 
 <template>
