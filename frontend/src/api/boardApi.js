edabfd9a9ce5d1e955f10fd28f51970cfe17af2d
Whatdoyumin <lym4963@naver.com>
2025-07-02 11:25:29 +0900

    feat: 게시글 생성 구현

--- frontend/src/api/boardApi.js
+++ frontend/src/api/boardApi.js
@@ -1,12 +1,30 @@
 import api from '@/api';
 
 const BASE_URL = '/api/board';
+const headers = { 'Content-Type': 'multipart/form-data' };
 
 export default {
   async getList(params) {
     const { data } = await api.get(BASE_URL, { params });
     console.log('BOARD GET LIST: ', data);
 
+    return data;
+  },
+
+  async create(article) {
+    const formData = new FormData();
+    formData.append('title', article.title);
+    formData.append('writer', article.writer);
+    formData.append('content', article.content);
+
+    if (article.files) {
+      for (let i = 0; i < article.files.length; i++) {
+        formData.append('files', article.files[i]);
+      }
+    }
+
+    const { data } = await api.post(BASE_URL, formData, { headers });
+    console.log('BOARD POST: ', data);
     return data;
   },
 };
