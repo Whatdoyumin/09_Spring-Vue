0f14a00916c87133c2197b626ad157568d5d5c69
Whatdoyumin <lym4963@naver.com>
2025-07-08 09:53:10 +0900

    fix: TravelMapper 및 TravelService 빈 등록을 위한 MapperScan 및 ComponentScan 설정 추가

--- backend/src/main/java/org/scoula/config/RootConfig.java
+++ backend/src/main/java/org/scoula/config/RootConfig.java
@@ -19,8 +19,10 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @Configuration
 @PropertySource({"classpath:/application.properties"})
-@MapperScan(basePackages = {"org.scoula.board.mapper", "org.scoula.member.mapper"})
-@ComponentScan(basePackages = {"org.scoula.board.service", "org.scoula.member.service"})
+@MapperScan(basePackages = {"org.scoula.board.mapper", "org.scoula.member.mapper",
+    "org.scoula.travel.mapper"})
+@ComponentScan(basePackages = {"org.scoula.board.service", "org.scoula.member.service",
+    "org.scoula.travel.service"})
 @Log4j2
 @EnableTransactionManagement
 public class RootConfig {
